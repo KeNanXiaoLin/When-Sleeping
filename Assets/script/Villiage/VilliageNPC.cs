2f8342aa606c23f99ebb66710b88efc928307750
CXwinter <CXknife@qq.com>
2025-10-05 14:50:40 +0800

    对话系统已接入

--- Assets/script/Villiage/VilliageNPC.cs
+++ Assets/script/Villiage/VilliageNPC.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 
 namespace GJ
 {
-
     public class VilliageNPC : MonoBehaviour
     {
         public LA_Item Item;
-        
+        public DialogData data;
+
+        public bool IsDialogued = false;
+
+        void Awake()
+        {
+            EventListener.OnDialogueStart += StartDialogue;
+        }
+
         public void PlayerGotItem()
         {
             LA_Backpack.instence.ShowGotItemUI_Backpack(Item);
+        }
+
+        private void StartDialogue()
+        {
+            if (IsDialogued == false)
+            {
+                IsDialogued = true;
+                DialogSystem.Instance.TriggerStartDialog(data);
+            }
+            else
+            {
+                EventListener.DialogueEnd();
+            }
         }
     }
 
