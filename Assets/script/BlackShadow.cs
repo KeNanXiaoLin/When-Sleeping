fccb94a781efe9b53281653097222ec512442315
CXwinter <CXknife@qq.com>
2025-10-05 19:43:12 +0800

    完成“黑雾”单位

---
+++ Assets/script/BlackShadow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using GJ;
+using UnityEngine;  
+
+public class BlackShadow : MonoBehaviour
+{
+    [SerializeField] private float MoveSpeed;
+
+    void FixedUpdate()
+    {
+        this.transform.Translate(new Vector2(1, 0) * MoveSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() == true)
+        {
+            EventListener.GameLose();
+        }
+    }
+}
